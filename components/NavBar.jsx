215e051cb098162619d427b352fdc2805528f192
ezizomer99 <ezizo18@uia.no>
2023-09-17 11:47:24 +0200

    update navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,15 +1,18 @@
 "use client"
 import React, {useState, useEffect} from 'react'
-
 import Image from 'next/image'
 import Link from 'next/link'
 import { AiOutlineClose, AiOutlineMenu, AiFillMail} from 'react-icons/ai'
 import { BsLinkedin, BsGithub} from 'react-icons/bs'
+import { useRouter } from 'next/router'
 
 const NavBar = () => {
 
     const [nav, setNav] = useState(false)
     const [shadow, setShadow] = useState(false)
+    const [navBg, setNavBg] = useState('#ecf0f3')
+    const [linkColor, setLinkColor] = useState('#1f2937')
+    
 
     const handleNav = () => {
         setNav(!nav);
@@ -27,16 +30,21 @@ const NavBar = () => {
     }, []);
 
   return (
-    <div className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 shadow-xl z-[100]"}>
+    <div 
+        style={{backgroundColor: `${navBg}`}}
+        className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 shadow-xl z-[100]"}>
         <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-            <Image
-                src="/assets/logo.png" 
-                alt="/"
-                width="110" 
-                height="50" 
-            />
+            <Link href='/'>
+                <Image
+                    src="/assets/logo.png" 
+                    alt="/"
+                    width="110" 
+                    height="50" 
+                />
+            </Link>
+            
             <div>
-                <ul className='hidden md:flex'>
+                <ul style={{color: `${linkColor}`}} className='hidden md:flex'>
                     <Link href='/'>
                         <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
                     </Link>
